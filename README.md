a1a4a293b715b0539d17f8b45c2d31d3ddf3eb13
shushuTona <42406967+shushuTona@users.noreply.github.com>
2023-02-20 19:22:12 +0900

    update README & create ECR

--- README.md
+++ README.md
@@ -5,3 +5,27 @@ AWS CDK（v2）をTSで書いてみる。
 - [AWS CDK とは](https://docs.aws.amazon.com/ja_jp/cdk/v2/guide/home.html)
 - [API Reference · AWS CDK](https://docs.aws.amazon.com/cdk/api/v2/docs/aws-construct-library.html)
 - [aws-samples/aws-cdk-examples](https://github.com/aws-samples/aws-cdk-examples/tree/master/typescript)
+
+## bootstrap
+
+```sh
+cdk bootstrap aws://【ACCOUNT-NUMBER】/【REGION】
+```
+
+## deploy
+
+```
+cdk deploy
+```
+
+## diff
+
+```sh
+cdk diff
+```
+
+## destroy the service created by cdk
+
+```sh
+cdk destroy
+```
--- cdk_aws_batch/src/lib/cdk_aws_batch-stack.ts
+++ cdk_aws_batch/src/lib/cdk_aws_batch-stack.ts
@@ -1,48 +1,66 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { Vpc, IpAddresses, SubnetType } from 'aws-cdk-lib/aws-ec2';
+// import { Vpc, IpAddresses, SubnetType } from 'aws-cdk-lib/aws-ec2';
 import { aws_ecr as ecr } from 'aws-cdk-lib';
 
-import { createComputeEnvironment } from '../resources/computeEnvironment';
-import { createJobDefinition } from '../resources/jobDefinition';
-import { createJobQueue } from '../resources/jobQueue';
-import { createRepository } from '../resources/repository';
+// import { createComputeEnvironment } from '../resources/computeEnvironment';
+// import { createJobDefinition } from '../resources/jobDefinition';
+// import { createJobQueue } from '../resources/jobQueue';
+// import { createRepository } from '../resources/repository';
 
 export class CdkAwsBatchStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // CREATE VPC
-    // https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_ec2.Vpc.html
-    // https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_ec2.CfnVPC.html
-    const vpc = new Vpc( this, 'cdk_aws_batch_vpc',
-                            {
-                              ipAddresses: IpAddresses.cidr( '10.10.0.0/16' ),
-                              maxAzs: 3,
-                              subnetConfiguration: [
-                                {
-                                  cidrMask: 24,
-                                  name: 'public-subnet',
+    /**
+     * create VPC
+     * https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_ec2.Vpc.html
+     * https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_ec2.CfnVPC.html
+     */
+    // const vpc = new Vpc( this, 'cdk_aws_batch_vpc',
+    //                         {
+    //                           ipAddresses: IpAddresses.cidr( '10.10.0.0/16' ),
+    //                           maxAzs: 3,
+    //                           subnetConfiguration: [
+    //                             {
+    //                               cidrMask: 24,
+    //                               name: 'public-subnet',
 
-                                  // Subnet Types
-                                  // https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_ec2-readme.html#subnet-types
-                                  subnetType: SubnetType.PUBLIC
-                                }
-                              ]
-                            }
-                          );
+    //                               // Subnet Types
+    //                               // https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_ec2-readme.html#subnet-types
+    //                               subnetType: SubnetType.PUBLIC
+    //                             }
+    //                           ]
+    //                         }
+    //                       );
 
-    // create ECR repository
-    const TestRepository = createRepository(this);
+    /**
+     * create ECR repository
+     * https://docs.aws.amazon.com/ja_jp/AmazonECR/latest/userguide/what-is-ecr.html
+     */
+    const testRepositoryProps: cdk.aws_ecr.RepositoryProps = {
+      repositoryName: 'test-repository',
+
+      // encryption:
+      // encryptionKey:
+      // lifecycleRules: ,
+      // lifecycleRegistryId:
+      // removalPolicy:
+
+      imageScanOnPush: true,
+
+      // imageTagMutability:
+    }
+    const TestRepository = new ecr.Repository(this, 'TestRepository', testRepositoryProps);
 
     // create batch ComputeEnvironment
-    const TestComputeEnvironment = createComputeEnvironment(this);
+    // const TestComputeEnvironment = createComputeEnvironment(this);
 
     // create JobDefinition
-    const TestJobDefinition = createJobDefinition(this);
+    // const TestJobDefinition = createJobDefinition(this);
 
     // class CfnJobQueue
-    const TestJobQueue = createJobQueue(this);
+    // const TestJobQueue = createJobQueue(this);
 
     // class CfnRule
     // https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_events.CfnRule.html
